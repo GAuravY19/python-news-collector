348cc081315d39c353e817510a3e74d8ebded74d
gaurav <gauravy1905@gmail.com>
2025-04-23 17:17:14 +0530

    Added Simulation to fixed IP

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ def main(source:list):
 
 
 def fetchAndSaveToFile(url, path):
-    r = requests.get(url)
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
+
+    r = requests.get(url, headers)
     with open(path, 'w', encoding='utf-8') as f:
         f.write(r.text)
 
