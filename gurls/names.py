1777507d538f8dfc945630791c4486a083711c10
gaurav <gauravy1905@gmail.com>
2025-04-24 09:14:50 +0530

    New directory for google related urls

---
+++ gurls/names.py
@@ -0,0 +1,58 @@
+NAMES_Aditya = 'Aditya Birla Health Insurance Company Limited'
+NAMES_Care = 'Care Health Insurance Company Limited'
+NAMES_ManipalCigna = 'ManipalCigna Health Insurance Company Limited'
+NAMES_Niva = 'Niva Bupa Health Insurance Company Limited'
+NAMES_Star = 'Star Health & Allied Insurance Company Limited'
+NAMES_Acko = 'Acko General Insurance Limited'
+NAMES_Bajaj = 'Bajaj Allianz General Insurance Company Limited'
+NAMES_Cholamandalam = 'Cholamandalam MS General Insurance Company Limited'
+NAMES_Future = 'Future Generali India Insurance Company Limited'
+NAMES_Go = 'Go Digit General Insurance Limited'
+NAMES_HDFC = 'HDFC Ergo General insurance Company Limited'
+NAMES_ICICI = 'ICICI Lombard General Insurance Company Limited'
+NAMES_IFFCO = 'IFFCO Tokio General Insurance Company Limited'
+NAMES_Kotak = 'Kotak Mahindra General Insurance Company Limited'
+NAMES_Kshema = 'Kshema General Insurance Company Limited'
+NAMES_Liberty = 'Liberty General Insurance Limited'
+NAMES_Magma = 'Magma HDI General Insurance Company Limited'
+NAMES_National = 'National Insurance Company Limited'
+NAMES_Raheja = 'Raheja QBE General Insurance Company Limited'
+NAMES_Reliance = 'Reliance General Insurance Company Limited'
+NAMES_Royal = 'Royal Sundaram General Insurance Company Limited'
+NAMES_SBI = 'SBI General Insurance Company Limited'
+NAMES_Shriram = 'Shriram General Insurance Company Limited'
+NAMES_Tata = 'Tata AIG General Insurance Company Limited'
+NAMES_The_new_india = 'The New India Assurance Company Limited'
+NAMES_Oriental = 'The Oriental Insurance Company Limited'
+NAMES_United = 'United India Insurance Company Limited'
+NAMES_Universal = 'Universal Sompo General Insurance Company Limited'
+NAMES_Zuno = 'Zuno General Insurance Limited'
+
+NAMES = [NAMES_Aditya,
+         NAMES_Care,
+         NAMES_ManipalCigna,
+         NAMES_Niva,
+         NAMES_Reliance,
+         NAMES_Star,
+         NAMES_Acko,
+         NAMES_Bajaj,
+         NAMES_Cholamandalam,
+         NAMES_Future,
+         NAMES_Go,
+         NAMES_HDFC,
+         NAMES_ICICI,
+         NAMES_IFFCO,
+         NAMES_Kotak,
+         NAMES_Kshema,
+         NAMES_Liberty,
+         NAMES_National,
+         NAMES_Magma,
+         NAMES_Raheja,
+         NAMES_Royal,
+         NAMES_Shriram,
+         NAMES_SBI,
+         NAMES_Tata,
+         NAMES_The_new_india,
+         NAMES_Oriental,
+         NAMES_Universal,
+         NAMES_Zuno]
---
+++ gurls/paths.py
@@ -0,0 +1,35 @@
+PATH_ADITYA = 'google_data/aditya.html'
+PATH_CARE = 'google_data/care.html'
+PATH_MANIPAL = 'google_data/manipal.html'
+PATH_NIVA = 'google_data/niva.html'
+PATH_RELIANCE = 'google_data/reliance.html'
+PATH_STAR = 'google_data/star.html'
+PATH_ACKO = 'google_data/acko.html'
+PATH_BAJAJ = 'google_data/bajaj.html'
+PATH_CHOLA = 'google_data/chola.html'
+PATH_FUTURE = 'google_data/future.html'
+PATH_GO = 'google_data/go.html'
+PATH_HDFC = 'google_data/hdfc.html'
+PATH_ICICI = 'google_data/icici.html'
+PATH_IFFCO = 'google_data/iffco.html'
+PATH_Kotak = 'google_data/kotak.html'
+PATH_Kshema = 'google_data/kshema.html'
+PATH_Liberty = 'google_data/liberty.html'
+PATH_National = 'google_data/national.html'
+PATH_Magma = 'google_data/magma.html'
+PATH_Raheja = 'google_data/rahega.html'
+PATH_Royal = 'google_data/royal.html'
+PATH_Shriram = 'google_data/shriram.html'
+PATH_SBI = 'google_data/sbi.html'
+PATH_Tata = 'google_data/tata.html'
+PATH_The_New_India = 'google_data/new_india.html'
+PATH_Oriental = 'google_data/Oriental.html'
+PATH_Universal = 'google_data/universal.html'
+PATH_Zuno = 'google_data/zuno.html'
+
+
+PATHS = [PATH_ADITYA, PATH_CARE, PATH_MANIPAL, PATH_NIVA, PATH_RELIANCE, PATH_STAR, PATH_ACKO, PATH_BAJAJ, PATH_CHOLA,
+         PATH_FUTURE, PATH_GO, PATH_HDFC, PATH_ICICI, PATH_IFFCO, PATH_Kotak, PATH_Kshema, PATH_Liberty, PATH_National,
+         PATH_Magma, PATH_Raheja, PATH_Royal, PATH_Shriram, PATH_SBI, PATH_Tata, PATH_The_New_India, PATH_Oriental,
+         PATH_Universal, PATH_Zuno]
+
